96445850e3ddf59bb27739ad9b93c6cb7f211694
mconwa02 <mconwa02@dcs.bbk.ac.uk>
2021-02-09 00:18:02 +0000

    adding test df

--- unit_Tests/iv_unit_tests.py
+++ unit_Tests/iv_unit_tests.py
@@ -1,7 +1,12 @@
-import IV_function.IV_code as iv
 import pandas as pd
-import numpy as np
-import config
-import os
+
+# pytest.fixtue
+test_df = pd.DataFrame({
+    'A': [123, 23, 100],
+    'B': [24, 12, 12],
+    'C': [23, 1, 22]
+})
+
+print(test_df)
 
 
